e61b44176ff5a012cdb3c40c7023b575f44b2db7
Eduardo”git config --global user.name Eduardo” <deanesa@yahoo.com>
2024-03-31 09:23:46 -0300

    yup date

--- main.js
+++ main.js
@@ -1,21 +1,33 @@
 
-// Create canvas variable
- blockY=1;
- blockX=1;
+var canvas = new fabric.Canvas('myCanvas');
+ block_y=1;
+ block_x=1;
 
-blockImageWidth = 350;
-blockImageHeight = 430;
+block_image_width = 350;
+block_image_height = 430;
 
-var blockImageObject= "";
+var block_image_object= "";
 
-function newImage(getImage)
+function new_image(get_image)
 {
-	// para enviar imagens
+	fabric.Image.fromURL(get_image, function(Img) {
+	block_image_object = Img;
+		
+	block_image_object.scaleToWidth(block_image_width);
+	block_image_object.scaleToHeight(block_image_height);
+	block_image_object.set({
+	top:block_y,
+	left:block_x
+	});
+	canvas.add(block_image_object);
+
+	});
+
 }
 
-window.addEventListener("keydown", myKeyDown);
+window.addEventListener("keydown", my_keydown);
 
-function myKeyDown(e)
+function my_keydown(e)
 {
 keyPressed = e.keyCode;
 console.log(keyPressed);
@@ -23,27 +35,37 @@ console.log(keyPressed);
 	if(keyPressed == '?') // adicione os códigos adequados às teclas
 	{
 		// enviar ranger escarlate
+		new_image('rr1.png'); 
+		console.log("r");
 	}
+
 	if(keyPressed == '86')
 	{
 		blockX = 200;
-		// enviar ranger verde
+		// enviar ranger verd
+		 new_image('gr.png'); 
+		console.log("g");
 	}
 	
 	if(keyPressed == '65')
 	{
 		blockX =350;
 		// enviar ranger amarelo
+		 new_image('yr.png'); 
+		console.log("y");
 	}
 	if(keyPressed == '82')
 	{
 		blockX = 600;
 		// enviar ranger rosa
+		 new_image('pr.png'); 
+		console.log("p");
 	}
 	if(keyPressed == '73')
 	{
 		blockX = 700;
-	// enviar ranger índigo
+	// enviar ranger índigo new_image('br1.png'); 
+		console.log("b");
 	}
 	
 }
